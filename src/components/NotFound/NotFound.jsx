a078dadbffa34d2beb7996dbd4aa6fbe5d7fabbe
rashedrk <rashedrk001@gmail.com>
2023-04-12 23:47:24 +0600

    not found page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            404 Not found
+            <h2 className='font-bold text-3xl'>Sorry the page is Not Found</h2>
+            <h1 className='font-bold text-5xl
+            '>404</h1>
+            <img src="https://freefrontend.com/assets/img/tailwind-404-page-templates/404-page-not-found.png" alt="" />
         </div>
     );
 };
