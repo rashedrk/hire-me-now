c0c65ae1755318117060ce5512d95ce99f138191
rashedrk <rashedrk001@gmail.com>
2023-04-12 11:16:25 +0600

    jobs data added

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FeaturedJobs = () => {
+    return (
+        <div className='mb-10'>
+            <div className='text-center mb-6'>
+                <h2 className='font-bold text-xl'>Featured Jobs</h2>
+                <p className='text-sm text-[#757575]'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
--- src/components/JobCategoryList/JobCategoryList.jsx
+++ src/components/JobCategoryList/JobCategoryList.jsx
@@ -10,7 +10,7 @@ const JobCategoryList = () => {
     },[]);
 
     return (
-        <div>
+        <div className='mb-10'>
             <div className='text-center mb-6'>
             <h2 className='font-bold text-xl'>Job Category List</h2>
             <p className='text-sm text-[#757575]'>Explore thousands of job opportunities with all the information you need. Its your future</p>
