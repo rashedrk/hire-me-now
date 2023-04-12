54d275c8d18696898b9804730bc664525065499f
rashedrk <rashedrk001@gmail.com>
2023-04-12 23:49:48 +0600

    blog page

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Details from './components/Details/Details'
 import appliedJobsLoader from './utilities/appliedJobLoader'
 import Statistics from './components/Statistics/Statistics'
 import NotFound from './components/NotFound/NotFound'
+import Blog from './components/Blog/Blog'
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,10 @@ const router = createBrowserRouter([
       {
         path:"statistics",
         element: <Statistics></Statistics>
+      },
+      {
+        path:'blog',
+        element:<Blog></Blog>
       },
       {
         path: '*',
